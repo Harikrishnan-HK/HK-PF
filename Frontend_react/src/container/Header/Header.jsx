7e13c371aea44091d3931fc94dc2f2244e5a140b
harikrishnan-hk <krishnanhari@outlook.com>
2024-09-01 17:40:25 -0400

    resume download enabled

--- Frontend_react/src/container/Header/Header.jsx
+++ Frontend_react/src/container/Header/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import {TypeAnimation} from 'react-type-animation';
-import {motion} from 'framer-motion';
-import { images  } from '../../constants';
-import './Header.scss';
-import {AppWrap} from '../../wrapper';
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+import "./Header.scss";
+import { AppWrap } from "../../wrapper";
 
 // const scaleVariants = {
 //   whileInView: {
@@ -17,51 +17,46 @@ import {AppWrap} from '../../wrapper';
 // }
 const Header = () => {
   const navigateToAbout = () => {
-    window.location.href = './#about';
+    window.location.href = "./#about";
   };
   const downloadResume = () => {
-    window.open('/assetss/HkResume.pdf', '_blank');
+    window.open("/HariResume.pdf", "_blank");
   };
-   return (
-    <div className='app__headr'>
-      <div className='.app__headerName'>
+  return (
+    <div className="app__headr">
+      <div className=".app__headerName">
         <h1>Harikrishnan Karuppiah</h1>
-        <h2>I'm a
+        <h2>
+          I'm a
           <TypeAnimation
             sequence={[
-            'Web Developer',
-            2000, 
-            'DevOps Engineer',
-            2000,
-            'Cloud Solution Architect',
-            2000,
-            'Coder',
-            2000
+              "Web Developer",
+              2000,
+              "DevOps Engineer",
+              2000,
+              "Cloud Solution Architect",
+              2000,
+              "Coder",
+              2000,
             ]}
-          wrapper='span'
-          speed={50}
-          cursor={true}
-          style={{ fontSize: '1em', color: '#40128B', paddingLeft:'5px'  }}
-          repeat={Infinity} /> 
+            wrapper="span"
+            speed={50}
+            cursor={true}
+            style={{ fontSize: "1em", color: "#40128B", paddingLeft: "5px" }}
+            repeat={Infinity}
+          />
         </h2>
       </div>
 
       <div class="btn-container">
-          <button class="btn btn-color-1" onClick={navigateToAbout}>
-            Explore My Story
-          </button>
-          <button
-            class="btn btn-color-2"
-            onClick={downloadResume}
-          >
-            Download Resume
-          </button>
-
+        <button class="btn btn-color-1" onClick={navigateToAbout}>
+          Explore My Story
+        </button>
+        <button class="btn btn-color-2" onClick={downloadResume}>
+          Download Resume
+        </button>
       </div>
-    
-
     </div>
-
 
     // <div className='app__headr' >
     //   <motion.div
@@ -83,35 +78,35 @@ const Header = () => {
     //       </div>
     //     </div>
     //   </motion.div>
-      // <motion.div
-      //   whileInView={{opacity:[0,1]}}
-      //   transition={{duration:0.5, delayChildren:0.5}}
-      //   className='app__header-img'
-      // >
-      //   <img src={images.profile} alt="profile_bg" />
-      //   <motion.img
-      //   whileInView={{scale:[0,1]}}
-      //   transition={{duration:1, ease:'easeInOut'}}
-      //   src={images.circle}
-      //   alt='profile_circle'
-      //   className='overlay_circle'
-      //   />
-      // </motion.div>
-      // <motion.div
-      //   variant={scaleVariants}
-      //   whileInView={scaleVariants.whileInView}
-      //   transition={{duration:0.5}}
-      //   className='app__header-circles'
-      // >
-      //   {[images.flutter, images.redux, images.sass].map((circle,index)=>(
-      //     <div className='circle-cmp app__flex' key={`circle-${index}`}>
-      //       <img src={circle} alt="circle"/>
+    // <motion.div
+    //   whileInView={{opacity:[0,1]}}
+    //   transition={{duration:0.5, delayChildren:0.5}}
+    //   className='app__header-img'
+    // >
+    //   <img src={images.profile} alt="profile_bg" />
+    //   <motion.img
+    //   whileInView={{scale:[0,1]}}
+    //   transition={{duration:1, ease:'easeInOut'}}
+    //   src={images.circle}
+    //   alt='profile_circle'
+    //   className='overlay_circle'
+    //   />
+    // </motion.div>
+    // <motion.div
+    //   variant={scaleVariants}
+    //   whileInView={scaleVariants.whileInView}
+    //   transition={{duration:0.5}}
+    //   className='app__header-circles'
+    // >
+    //   {[images.flutter, images.redux, images.sass].map((circle,index)=>(
+    //     <div className='circle-cmp app__flex' key={`circle-${index}`}>
+    //       <img src={circle} alt="circle"/>
 
-      //     </div>
-      //   ))}
-      // </motion.div> 
+    //     </div>
+    //   ))}
+    // </motion.div>
     // </div>
-  )
-}
+  );
+};
 
-export default AppWrap(Header, 'home');
+export default AppWrap(Header, "home");
