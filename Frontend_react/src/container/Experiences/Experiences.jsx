3e698c5cd2fcaa9ebdd6b8c155927dc608906462
harikrishnan-hk <krishnanhari@outlook.com>
2024-09-03 18:28:26 -0400

    experience desc line-4 added

--- Frontend_react/src/container/Experiences/Experiences.jsx
+++ Frontend_react/src/container/Experiences/Experiences.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import { Tooltip } from 'react-tooltip';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { Tooltip } from "react-tooltip";
 
-import { AppWrap,MotionWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
-import './Experiences.scss';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+import "./Experiences.scss";
 
 const Experiences = () => {
   const [experiences, setExperiences] = useState([]);
@@ -15,7 +15,6 @@ const Experiences = () => {
     client.fetch(query).then((data) => {
       setExperiences(data);
     });
-
   }, []);
 
   return (
@@ -23,16 +22,12 @@ const Experiences = () => {
       <h2 className="head-text">My Work Experience</h2>
 
       <div className="app__skills-container">
-
         <div className="app__skills-exp">
           {experiences.map((experience) => (
-            <motion.div
-              className="app__skills-exp-item"
-              key={experience.year}
-            >
+            <motion.div className="app__skills-exp-item" key={experience.year}>
               <div className="app__skills-exp-year">
                 <p className="bold-text">{experience.year}</p>
-                </div>
+              </div>
               <motion.div className="app__skills-exp-works">
                 {experience.works.map((work) => (
                   <>
@@ -45,10 +40,16 @@ const Experiences = () => {
                       key={work.name}
                     >
                       <h3 className="bold-text">{work.name}</h3>
-                      <h4 className="bold-text" style={{color:`var(--secondary-color)`}}>{work.company}</h4>
+                      <h4
+                        className="bold-text"
+                        style={{ color: `var(--secondary-color)` }}
+                      >
+                        {work.company}
+                      </h4>
                       <p className="p-text">{work.desc1}</p>
                       <p className="p-text">{work.desc2}</p>
                       <p className="p-text">{work.desc3}</p>
+                      <p className="p-text">{work.desc4}</p>
                     </motion.div>
 
                     {/* <Tooltip
@@ -62,12 +63,10 @@ const Experiences = () => {
                   </>
                 ))}
               </motion.div>
-                {experience.works.map((work) => (
-                  
-              <img src={urlFor(work.icon)} alt={work.name} />
-                ))}
-              </motion.div>
-
+              {experience.works.map((work) => (
+                <img src={urlFor(work.icon)} alt={work.name} />
+              ))}
+            </motion.div>
           ))}
         </div>
         {/* <div className="app__skills-container">
@@ -90,13 +89,13 @@ const Experiences = () => {
           ))}
         </motion.div>
         </div > */}
-
-
       </div>
     </>
   );
 };
 
-
 export default AppWrap(
-  MotionWrap(Experiences, 'app__skills'), 'experiences', "app__primarybg");
+  MotionWrap(Experiences, "app__skills"),
+  "experiences",
+  "app__primarybg"
+);
--- backend_sanity/schemas/workExperience.js
+++ backend_sanity/schemas/workExperience.js
@@ -1,40 +1,41 @@
 export default {
-    name:'workExperience',
-    title:'Work Experience',
-    type:'document',
-    fields:[
-           {name:'name',
-               title:'name',
-               type:'string'
-            },
-            {
-                name:'company',
-                title:'Company',
-                type:'string'
-            },
-            {
-                name:'desc1',
-                title:'Desc Line1',
-                type:'string'
-            },
-            {
-                name:'desc2',
-                title:'Desc Line2',
-                type:'string'
-            },
-            {
-                name:'desc3',
-                title:'Desc Line3',
-                type:'string'
-            },
-            {
-                name:'icon',
-                title:'Company Logo',
-                type: 'image',
-                options: {
-                  hotspot: true,
-                },
-            },
-    
-    ]
+  name: 'workExperience',
+  title: 'Work Experience',
+  type: 'document',
+  fields: [
+    {name: 'name', title: 'name', type: 'string'},
+    {
+      name: 'company',
+      title: 'Company',
+      type: 'string',
+    },
+    {
+      name: 'desc1',
+      title: 'Desc Line1',
+      type: 'string',
+    },
+    {
+      name: 'desc2',
+      title: 'Desc Line2',
+      type: 'string',
+    },
+    {
+      name: 'desc3',
+      title: 'Desc Line3',
+      type: 'string',
+    },
+    {
+      name: 'desc4',
+      title: 'Desc Line4',
+      type: 'string',
+    },
+    {
+      name: 'icon',
+      title: 'Company Logo',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
 }
