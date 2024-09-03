da0aeaf6cd392b726b809b5bb30bee6b106295a7
harikrishnan-hk <krishnanhari@outlook.com>
2024-09-03 18:30:41 -0400

    Testimonial nav dot removed

--- Frontend_react/src/components/NavigationDots.jsx
+++ Frontend_react/src/components/NavigationDots.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const NavigationDots = ({active}) => {
+const NavigationDots = ({ active }) => {
   return (
-    <div className='app__navigation'>
-        {['home', 'about', 'projects','skills','experiences', 'education', 'testimonial', 'contact' ].map((item, index)=>(
-        <a 
-            href={`#${item}`} 
-            key = {item +index}
-            className='app__navigation-dot'
-            style = {active === item ? {backgroundColor : '#313BAC'} : {}}
-        /> 
-        ))}
+    <div className="app__navigation">
+      {[
+        "home",
+        "about",
+        "projects",
+        "skills",
+        "experiences",
+        "education",
+        "contact",
+      ].map((item, index) => (
+        <a
+          href={`#${item}`}
+          key={item + index}
+          className="app__navigation-dot"
+          style={active === item ? { backgroundColor: "#313BAC" } : {}}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default NavigationDots
+export default NavigationDots;
